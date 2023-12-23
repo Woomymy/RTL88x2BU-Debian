8a65bdcc83a597e932c5276bf113adc535cf2c0f
Woomymy <woomy@woomy.be>
2023-12-23 20:06:29 +0100

    build: Clone driver git repository
    Change-Id: Ic72a1942367de0e5e328205b75fcd9e6d66f7226
    Signed-off-by: Woomymy <woomy@woomy.be>

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 if [[ $UID != "0" ]]; then
 	echo -e "\e[91mYou need to run this script as root!"
 fi
+
+git clone "https://github.com/RinCat/RTL88x2BU-Linux-Driver.git" usr/src/rtl88x2bu
+
 dpkg-buildpackage
